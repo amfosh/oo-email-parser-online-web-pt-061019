903e323dfad7b58753234d9796572f2d4532bf71
Alicia Fasciocco <afasciocco@gmail.com>
2019-07-04 17:24:31 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,13 +1,13 @@
 class EmailParser
-  attr_accessor :emails
-  
-  @email = []
+  attr_accessor :email
 
-  def initialize(emails)
-   @email = @emails.split(/[,\s]+/)
-  end
+  @email = []
   
+  def initialize(emails)
+    @email = emails.split(/[,\s]+/)
+  end
+
   def parse
     @email.uniq
   end
- end 
+end
